f2e248e0a8fbf964c8b4592afdd433e228da93c0
Yashkumar Nemane <96227576+yashnemane@users.noreply.github.com>
2024-04-04 16:38:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 
+import express from 'express';
+import cors from 'cors';
 const express = require('express');
 const app = express()
 const mongoose = require('mongoose');
@@ -13,8 +15,10 @@ app.use((req,res,next)=>{
     next()
 })
 
+app.use(cors());
 app.use(function (req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next();
 })
