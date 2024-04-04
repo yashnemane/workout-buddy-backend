d2c3d8e719c6adf93b9af558c2ecd4daffd5fc07
Yashkumar Nemane <96227576+yashnemane@users.noreply.github.com>
2024-04-04 16:00:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.use((req,res,next)=>{
 app.use(function (req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   req.header("Access-Control-Allow-Methods": "GET, POST, PUT, DELETE");
    next();
 })
 
