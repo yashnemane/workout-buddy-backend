3bb9e3941b58bc9ec86952c59e91520c6f0e1894
Yashkumar Nemane <96227576+yashnemane@users.noreply.github.com>
2024-04-04 17:47:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use((req,res,next)=>{
     next()
 })
 
-app.use(cors());
 app.use(function (req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
