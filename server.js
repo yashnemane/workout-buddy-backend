811bfedd7813ef649b3e8f90d36701ca95a10f85
Yashkumar Nemane <96227576+yashnemane@users.noreply.github.com>
2024-04-04 18:08:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,16 +9,19 @@ const workoutRoutes = require('./routes/workouts');
 //middleware
 app.use(express.json())
 app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     console.log(req.path, req.method)
     next()
 })
 
-app.use(function (req, res, next) {
+/*app.use(function (req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next();
-})
+})*/
 
 //routes
 const backendBaseURL="https://workout-buddy-backend-pmfl.onrender.com"
