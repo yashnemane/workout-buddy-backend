3d844da5817198904e4e6bfcfe892e9f75066a87
Yashkumar Nemane <96227576+yashnemane@users.noreply.github.com>
2024-04-04 16:11:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 require('dotenv').config()
 
-const express = require('express')
-const mongoose = require('mongoose')
-const workoutRoutes = require('./routes/workouts')
-const cors=require('cors')
-
-//express app
 const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts');
+const cors = require('cors');
+
 
 //middleware
 app.use(express.json())
