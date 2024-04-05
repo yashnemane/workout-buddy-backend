e8ffedd3f8de5a0772abe20de000ff903e1c4931
Yashkumar Nemane <96227576+yashnemane@users.noreply.github.com>
2024-04-05 12:38:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const workoutRoutes = require('./routes/workouts');
 //middleware
 app.use(express.json())
 app.use((req,res,next)=>{
-    res.header("Access-Control-Allow-Origin", "https://workout-buddy-x.netlify.app/api/workouts/");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     console.log(req.path, req.method)
