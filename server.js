e11aab1a436f52751523feba2417acb98537cd83
Yashkumar Nemane <96227576+yashnemane@users.noreply.github.com>
2024-04-08 11:15:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,16 +4,10 @@ const express = require('express');
 const app = express()
 const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
-const cors = require('express-cors');
 
 
 //middleware
 app.use(express.json())
-app.use(cors({
-    allowedOrigins: [
-        '*'
-    ]
-}));
 app.use((req,res,next)=>{
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
